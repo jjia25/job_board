d5d6dc6cc02c28ec90cc1d4e3e9c8b2459dfd313
Joanna Jia <jjia25@users.noreply.github.com>
2017-02-27 11:22:44 -0800

    Update jobs_controller.rb
    Added a few notes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,11 @@
 class JobsController < ApplicationController
   before_action :find_job, only: [:edit, :update, :destroy]
+
   def index
     @jobs = Job.all
   end
 
+ #creates a new job
   def new
     @job = Job.new
   end
@@ -27,7 +29,7 @@ class JobsController < ApplicationController
     end
   end
 
-#methond [destroy]
+#method [destroy]
   def destroy
     @job.destroy
     redirect_to jobs_path
